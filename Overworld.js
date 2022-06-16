1cd645f11da3832583054a7f0b64a4a00f51518f
elliott dombrowski <elliottroyal@me.com>
2022-06-16 18:05:34 -0500

    refactored content drawing

--- Overworld.js
+++ Overworld.js
@@ -6,6 +6,7 @@ class Overworld {
     this.element = config.element;
     this.canvas = this.element.querySelector(".game-canvas");
     this.ctx = this.canvas.getContext("2d");
+    this.map = null;
   };
 
   //GAME LOOP
@@ -19,31 +20,9 @@ class Overworld {
   };
 
   init() {
-    // ON INIT, CREATE NEW INSTANCE OF IMAGE
-    // BROWSER MUST PRE-LOAD IMAGES IN MEMORY TO DISPLAY IN CANVAS
-
-    //MAP LOWER LAYER
-    const image = new Image();
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0);
-    };
-    image.src = "assets/size-test-export.png";
-
-    //PLACE GAME OBJECTS
-    const hero = new GameObject({
-      x: 8,
-      y: 6,
-    });
-    const npc1 = new GameObject({
-      x: 10,
-      y: 6,
-      src: '/assets/sprite-template.png'
-    });
-
-    setTimeout(() => {
-      //CALL DRAW FUNCT IN SPRITE.JS, DRAW SPRITE TO SCREEN
-      hero.sprite.draw(this.ctx);
-      npc1.sprite.draw(this.ctx);
-    }, 200);
+    //TELL OVERWORLD WHICH CONTENT MAP TO DRAW DATA FROM
+    this.map = new OverworldMap(window.OverworldMaps.ApartmentOne);
+    //ON INIT, CALL STARTGAMELOOP 
+    this.startGameLoop();
   };
 };
