7c5e3d150ddfe26548066bd79e25b8854be25c10
elliott dombrowski <elliottroyal@me.com>
2022-06-16 19:58:04 -0500

    held directions array checking

--- DirectionInput.js
+++ DirectionInput.js
@@ -1,11 +1,37 @@
 class DirectionInput {
   constructor() {
     this.heldDirections = [];
+
+    this.map = {
+      "KeyW": "up",
+      "KeyS": "down",
+      "KeyA": "left",
+      "KeyD": "right",
+      
+      "ArrowUp": "up",
+      "ArrowDown": "down",
+      "ArrowLeft": "left",
+      "ArrowRight": "right",
+    }
   }
 
   init() {
+    //TRACK INPUTS ENTERING HELDDIRECTIONS ARRAY
     document.addEventListener("keydown", e => {
-      console.log(e.code);
+      const dir = this.map[e.code]; //CHECK IF KEY PRESSED IS VALID INPUT
+
+      if (dir && this.heldDirections.indexOf(dir) === -1) {
+        this.heldDirections.unshift(dir);
+      };
+    });
+
+    //TRACK INPUTS EXITING HELDDIRECTIONS ARRAY
+    document.addEventListener("keyup", e => {
+      const dir = this.map[e.code];
+      const index = this.heldDirections.indexOf(dir);
+      if (index > -1) {
+        this.heldDirections.splice(index, 1);
+      };
     });
   };
 };
